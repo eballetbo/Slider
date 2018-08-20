694af7bab1326a1a9dfcac6024e138c26f59ee44
Enric Balletbo i Serra <eballetbo@gmail.com>
2018-08-20 18:48:34 +0200

    Wifi: don't wait for serial port to connect.
    Wait for serial port to connect is needed for native USB port only and
    debug purposes but we don't really want to wait for serial connection
    when the application is running standalone.
    
    Signed-off-by: Enric Balletbo i Serra <eballetbo@gmail.com>

--- Wifi.ino
+++ Wifi.ino
@@ -1,9 +1,9 @@
 void setupWiFi() {
    //Initialize serial and wait for port to open:
   Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  //while (!Serial) {
+  //  ; // wait for serial port to connect. Needed for native USB port only
+  //}
 
   Serial.println("Access Point Web Server");
 
